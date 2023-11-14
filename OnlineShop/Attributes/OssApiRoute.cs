b39c86929324cb25c93877a8de639a0f497ce6bc
stanislavov <mile.stanislavov@kontron.mk>
2023-11-14 14:28:04 +0100

    initial commit

---
+++ OnlineShop/Attributes/OssApiRoute.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Online.Shopping.Store.Attributes
+{
+    internal class OssApiRoute : RouteAttribute
+    {
+        public OssApiRoute(string template) : base($"/api/v1/{template}")
+        {
+
+        }
+    }
+}
---
+++ OnlineShop/Controllers/UserController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Online.Shopping.Store.Attributes;
+namespace Online.Shopping.Store.Controllers
+{
+    [ApiController]
+    [OssApiRoute("users")]
+    public class UserController : ControllerBase
+    {
+
+    }
+}
